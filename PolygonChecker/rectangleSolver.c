517e8ec9436585fa52e0a53496edd1f4238945ab
JameshWray <jwray2009@conestogac.on.ca>
2025-11-12 14:27:55 -0500

    add isLineOnAxis function

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -63,6 +63,21 @@ float dOfPoints(int pointP[], int pointQ[]) {
 	float distance = sqrtf((powf((float)deltaX, 2)) + (powf((float)deltaY, 2)));
 	return distance;
 }
+/*check if index of an array 0=1 or 2=3*/
+int isLineOnAxis(int array[]) {
+	if ((array[0] == array[1]) || (array[2] == array[4])) {
+		if ((array[0] == array[1]) || (array[2] == array[4])) {
+			return 2;
+		}
+		else {
+			return 1;
+		}
+
+	}
+	else {
+		return 0;
+	}
+}
 char* isRectangle(int pointA1, int pointA2, int pointB1, int pointB2, int pointC1, int pointC2, int pointD1, int pointD2) {
 	char* result = "";
 	
